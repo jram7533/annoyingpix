86f10caa4e57d8c0689aab52cdb453265ddd328f
Jack Ramsey <jram7533@gmail.com>
2026-04-15 10:45:58 -0400

    Add header comments to pix.js
    Added header comments with author, date, and description.

--- js/pix.js
+++ js/pix.js
@@ -1,4 +1,11 @@
+/** pix.js
+ *      Author:         
+ *      Date:           2025-11-12
+ *      Last revised:   
+ *      Description: JS for annoying pix in-class exercise
+ *      -> Uses annoyingpixapi.csci1720.net to fetch path
+ *         to next "annoying picture" in the api's archive
+ */
 // JS for annoying pix in-class
 let btn = document.querySelector("#btn");
 let pic = document.querySelector("#annoyingpic");
-
